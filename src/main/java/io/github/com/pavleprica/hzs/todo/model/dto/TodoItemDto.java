9fba1be17c124232836e7b01ed33fcda2d83d469
Pavle Prica <pavle.prica5@gmail.com>
2021-11-09 17:36:35 +0100

    Update TodoItemDto to record

--- src/main/java/io/github/com/pavleprica/hzs/todo/model/dto/TodoItemDto.java
+++ src/main/java/io/github/com/pavleprica/hzs/todo/model/dto/TodoItemDto.java
@@ -1,34 +1,24 @@
 package io.github.com.pavleprica.hzs.todo.model.dto;
 
 import io.github.com.pavleprica.hzs.todo.model.TodoItemType;
-import io.github.com.pavleprica.hzs.todo.model.entity.TodoItem;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
-@AllArgsConstructor
-@NoArgsConstructor
-@Builder
-@Data
-public class TodoItemDto {
+public record TodoItemDto(
+        @Min(1)
+        Long id,
 
-    @Min(1)
-    private Long id;
+        @NotBlank
+        @NotNull
+        String title,
 
-    @NotBlank
-    @NotNull
-    private String title;
+        @NotBlank
+        @NotNull
+        String description,
 
-    @NotBlank
-    @NotNull
-    private String description;
-
-    @NotNull
-    private TodoItemType todoItemType;
-
+        @NotNull
+        TodoItemType todoItemType
+) {
 }
