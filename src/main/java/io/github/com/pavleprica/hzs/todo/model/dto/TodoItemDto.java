c07ef1c66d8d455a9a3b54eb57fc75ab55d20b4a
Pavle Prica <pavle.prica5@gmail.com>
2021-11-11 17:11:38 +0100

    Add createdAt to TodoItemDto

--- src/main/java/io/github/com/pavleprica/hzs/todo/model/dto/TodoItemDto.java
+++ src/main/java/io/github/com/pavleprica/hzs/todo/model/dto/TodoItemDto.java
@@ -5,6 +5,7 @@ import io.github.com.pavleprica.hzs.todo.model.TodoItemType;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+import java.time.Instant;
 
 public record TodoItemDto(
         @Min(1)
@@ -19,6 +20,8 @@ public record TodoItemDto(
         String description,
 
         @NotNull
-        TodoItemType todoItemType
+        TodoItemType todoItemType,
+
+        Instant createdAt
 ) {
 }
